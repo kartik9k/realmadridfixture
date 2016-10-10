4b4b9b526efe3791c3c077f009ffb45945a02b11
Kartik Mathur <kartik@kartik-ThinkPad-E450>
2016-10-10 21:51:24 +0530

    added name of the competition

--- realFixture.py
+++ realFixture.py
@@ -9,7 +9,9 @@ awayTeam = bs4data.find('div', {'class': 'm_highlighted_next_game_team m_highlig
 venue = bs4data.find('p', {'class': 'm_highlighted_next_game_location'}).contents
 startTime = bs4data.find('time', {'class': 'm_highlighted_next_game_time'}).contents
 matchDate = bs4data.find('header', {'class': 'm_highlighted_next_game_header'}).time.contents
+competition = bs4data.find('header', {'class': 'm_highlighted_next_game_header'}).p.span.contents 
 	 
+print("The competition is : " + competition[0])
 print("The next fixture is: " + homeTeam[0] + " VS " + awayTeam[0])
 print("The venue is: " + venue[0])
 print("The date is: " + matchDate[0])
